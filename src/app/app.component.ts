f7614e981ee3fafb26337943c7a1d5ec44b21050
Marco Bassaletti <bassaletti@gmail.com>
2017-10-09 17:52:08 -0300

    Added demo component to test counter store.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import * as CounterActions from './counter.actions';
+import {AppStore} from './app.store';
+import {Store} from 'redux';
+import {AppState} from './app.state';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  counter: number;
+
+  constructor(@Inject(AppStore) private store: Store<AppState>) {
+    store.subscribe(() => this.readState());
+    this.readState();
+  }
+
+  readState(): void {
+    const state: AppState = this.store.getState() as AppState;
+    this.counter = state.counter;
+  }
+
+  increment(): void {
+    this.store.dispatch(CounterActions.increment());
+  }
+
+  decrement(): void {
+    this.store.dispatch(CounterActions.decrement());
+  }
 }
